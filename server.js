9dba47aecf3fb17138ef897e86212249a9372091
Karan <karan.l.k123321@gmail.com>
2025-04-10 20:52:30 +0530

    * Use ExpressLayout for Elements reusability

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
 
-//Assets
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 //set template
 
 app.use(expressLayout);
@@ -18,6 +12,17 @@ app.set("views", path.join(__dirname, "/resources/views"));
 
 //which template to use
 app.set("view engine", "ejs");
+
+//Assets
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+//cart
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
