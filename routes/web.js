6d0feec65cc611cbf0a462cfd7b900718a010dca
Karan <karan.l.k123321@gmail.com>
2025-04-13 18:44:35 +0530

    Update All Routing Controllers

--- routes/web.js
+++ routes/web.js
@@ -1,27 +1,23 @@
 // routes/web.js
 
+const authController = require("../app/http/controllers/authController");
+const cartController = require("../app/http/controllers/customers/cartController");
 const homeController = require("../app/http/controllers/homeController");
 
 function initRoutes(app) {
   //homeController().index; //calling homecontroller.index gives us the object
- 
+
   app.get("/", homeController().index);
 
   // (req, res) => {
   //   res.render("home");
   // }
 
-  app.get("/cart", (req, res) => {
-    res.render("customers/cart");
-  });
+  app.get("/cart", cartController().index);
 
-  app.get("/login", (req, res) => {
-    res.render("auth/login");
-  });
+  app.get("/login", authController().login);
 
-  app.get("/register", (req, res) => {
-    res.render("auth/register");
-  });
+  app.get("/register", authController().register);
 }
 
 // Export the function so it can be required in server.js
