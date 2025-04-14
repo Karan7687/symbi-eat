6acade9c01471f4f82dbeebd979a868052f204cd
Karan <karan.l.k123321@gmail.com>
2025-04-14 19:18:23 +0530

    Sessionand Cookie Setup

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
-
+const session = require("express-session");
 // Set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
@@ -32,6 +34,7 @@ connection.on("error", (err) => {
   console.log("Failed to connect to DB", err);
 });
 
+  
 // Server static files
 app.use(express.static("public"));
 
