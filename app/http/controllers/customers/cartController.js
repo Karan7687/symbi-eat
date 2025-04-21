d8773b808d054ff2a7dceea238a046a2aea8e493
Karan <karan.l.k123321@gmail.com>
2025-04-21 19:34:54 +0530

    Save cart into Session

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -14,9 +14,25 @@ function cartController() {
       }
 
       let cart = req.session.cart;
-      console.log(req.body);
 
-      return res.json({ data: "All okay" });
+      // console.log(req.body);
+
+      //chck if the item is not present in the cart
+      if (!cart.items[req.body._id]) {
+        cart.items[req.body._id] = {
+          item: req.body,
+          qty: 1,
+        };
+
+        cart.totalQty = cart.totalQty + 1;
+        cart.totalPrice = cart.totalPrice + req.body.price;
+      } else {
+        //if foodItem already present in the cart
+        cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1;
+        cart.totalQty = cart.totalQty + 1;
+        cart.totalPrice = cart.totalPrice + req.body.price;
+      }
+      return res.json({ totalQty: req.session.cart.totalQty });
       // send success response to frontend
     },
   };
