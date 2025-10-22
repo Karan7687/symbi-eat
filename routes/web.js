b5ada9511ccd7464428c955333f4d2b987f29ef4
Karan <karan.l.k123321@gmail.com>
2025-10-23 04:07:02 +0530

    Apply auth middleware on order section accessible only for logged in users

--- routes/web.js
+++ routes/web.js
@@ -4,7 +4,8 @@ const authController = require("../app/http/controllers/authController");
 const cartController = require("../app/http/controllers/customers/cartController");
 const orderController = require("../app/http/controllers/customers/orderController");
 const homeController = require("../app/http/controllers/homeController");
-const guest= require("../app/http/middlewares/guest");
+const guest = require("../app/http/middlewares/guest");
+const auth = require("../app/http/middlewares/auth");
 
 function initRoutes(app) {
   //homeController().index; //calling homecontroller.index gives us the object
@@ -15,19 +16,19 @@ function initRoutes(app) {
   //   res.render("home");
   // }
 
-  app.get("/login", guest,authController().login);
+  app.get("/login", guest, authController().login);
   app.post("/login", authController().postLogin);
   app.post("/logout", authController().logout);
 
-  app.get("/register", guest,authController().register);
+  app.get("/register", guest, authController().register);
   app.post("/register", authController().postRegister);
 
   app.get("/cart", cartController().index);
   app.post("/update-cart", cartController().update);
-  
+
   //Customer Routes
-  app.post("/orders",orderController().store);  
-  app.get("/customer/orders",orderController().index);
+  app.post("/orders",auth, orderController().store);
+  app.get("/customer/orders",auth, orderController().index);
 }
 
 // Export the function so it can be required in server.js
